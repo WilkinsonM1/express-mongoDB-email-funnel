26808a2cf67eb640bfc77e1aa04a57bffeb94fab
CN72 <cn72@CN72.local>
2019-10-11 15:27:09 +0100

    connected register form with server and database

---
+++ public/app.js
@@ -0,0 +1,30 @@
+const loginForm = document.getElementById('login-form');
+const nameInp= document.getElementById('name-inp');
+const passwordInp= document.getElementById('password-inp');
+const loginBtn = document.getElementById('login-btn');
+const registerBtnInp = document.getElementById('register-btn-inp');
+
+const registerForm = document.getElementById('register-form');
+const nameRegisterInp = document.getElementById('name-register-inp');
+const emailRegisterInp = document.getElementById('email-register-inp')
+const passwordRegisterInp= document.getElementById('password-register-inp');
+const registerBtn = document.getElementById('register-btn');
+
+// registerBtn.addEventListener('click', async (e) => {
+//     e.preventDefault();
+//   await fetch('http://localhost:3000/register', {
+//        medthod: "POST",
+//        headers: {"content-type": "application/json"},
+//        body: JSON.stringify({
+//         name: nameRegisterInp.value,
+//         email: emailRegisterInp.value,
+//         password: passwordRegisterInp.value
+//     })
+
+
+//    });
+//    console.log('button works')
+// //    window.location.reload();
+// });
+
+
